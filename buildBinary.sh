6cc41095951da31a15b51f839582bd5008ec094d
pinak3748 <pinakfaldu123@gmail.com>
2024-10-14 00:20:32 +0530

    feat: Added binary for testing and updated readme

---
+++ buildBinary.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set the version
+VERSION="v0.1"
+
+# Create binary folder if it doesn't exist
+mkdir -p binary
+
+echo "Building Nox for Windows..."
+GOOS=windows GOARCH=amd64 go build -o binary/nox_${VERSION}_windows_amd64.exe
+
+echo "Building Nox for Mac (Intel)..."
+GOOS=darwin GOARCH=amd64 go build -o binary/nox_${VERSION}_mac_amd64
+
+echo "Building Nox for Mac (M1)..."
+GOOS=darwin GOARCH=arm64 go build -o binary/nox_${VERSION}_mac_arm64
+
+echo "Build complete! Binaries stored in the 'binary' folder."
