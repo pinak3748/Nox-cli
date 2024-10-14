8973f33ac587579aeb469ef374df69f696daf289
pinak3748 <pinakfaldu123@gmail.com>
2024-10-14 23:02:30 +0530

    feat: Added dynamic loader and improved init command

--- go.mod
+++ go.mod
@@ -5,6 +5,34 @@ go 1.23.0
 require github.com/spf13/cobra v1.8.1
 
 require (
+	github.com/alecthomas/chroma/v2 v2.14.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/briandowns/spinner v1.23.1 // indirect
+	github.com/charmbracelet/glamour v0.8.0 // indirect
+	github.com/charmbracelet/lipgloss v0.12.1 // indirect
+	github.com/charmbracelet/x/ansi v0.1.4 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.27 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/yuin/goldmark v1.7.4 // indirect
+	github.com/yuin/goldmark-emoji v1.0.3 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/term v0.22.0 // indirect
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/text v0.19.0
--- packages/init.go
+++ packages/init.go
@@ -4,10 +4,28 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/manifoldco/promptui"
+)
+
+type ProjectType string
+type PackageManager string
+
+const (
+	React_JS ProjectType    = "react + js"
+	React_TS ProjectType    = "react + ts"
+	NPM      PackageManager = "npm"
+	Yarn     PackageManager = "yarn"
+	Bun      PackageManager = "bun"
+	Pnpm     PackageManager = "pnpm"
 )
 
 type Project struct {
-	Name string
+	Name           string
+	ProjectType    ProjectType
+	PackageManager PackageManager
 }
 
 func Init(projectName string) {
@@ -17,14 +35,122 @@ func Init(projectName string) {
 		projectName = "client"
 	}
 
+	var basePath = projectName + "/"
+	var projectType ProjectType
+	var packageManager PackageManager
+
+	// Ask the user to choose a project type
+	projectTypePrompt := promptui.Select{
+		Label: "🤔 What type of project would you like to create?",
+		Items: []string{"React + JavaScript", "React + TypeScript"},
+	}
+
+	_, projectTypeChoice, err := projectTypePrompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	switch projectTypeChoice {
+	case "React + JavaScript":
+		projectType = React_JS
+	case "React + TypeScript":
+		projectType = React_TS
+	default:
+		fmt.Println("⚠️ Invalid choice. Defaulting to React + JavaScript.")
+		projectType = React_JS
+	}
+
+	fmt.Println("🚧 Creating a new", projectType, "project...")
+
+	// Ask the user to choose a package manager
+	packageManagerPrompt := promptui.Select{
+		Label: "🤔 Which package manager would you like to use?",
+		Items: []string{"npm", "yarn", "bun", "pnpm"},
+	}
+
+	_, packageManagerChoice, err := packageManagerPrompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	switch packageManagerChoice {
+	case "npm":
+		packageManager = NPM
+	case "yarn":
+		packageManager = Yarn
+	case "bun":
+		packageManager = Bun
+	case "pnpm":
+		packageManager = Pnpm
+	default:
+		fmt.Println("⚠️ Invalid choice. Defaulting to npm.")
+		packageManager = NPM
+	}
+
 	// Clone a repository
 	var githubRepo = "https://github.com/Ionio-io/seed-ui.git"
 
-	var gitCloneCmd = exec.Command("git", "clone", githubRepo, projectName)
+	// Check if the "client" directory exists at the root level
+	if _, err := os.Stat("client"); os.IsNotExist(err) {
 
-	err := gitCloneCmd.Run()
+		var gitCloneCmd = exec.Command("git", "clone", githubRepo, projectName)
+
+		err = gitCloneCmd.Run()
+		if err != nil {
+			fmt.Println("⚠️ Oops! Something went wrong while cloning the repository. Check your connection and try again.")
+			os.Exit(1)
+		}
+	}
+
+	// Installing dependencies
+	var installCmd *exec.Cmd
+
+	switch packageManager {
+	case NPM:
+		installCmd = exec.Command("npm", "install")
+	case Yarn:
+		installCmd = exec.Command("yarn", "install")
+	case Bun:
+		installCmd = exec.Command("bun", "install")
+	case Pnpm:
+		installCmd = exec.Command("pnpm", "install")
+	}
+
+	installCmd.Dir = basePath
+
+	// Start the spinner
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	var Suffixes = []string{
+		"🚚 Installing dependencies...",
+		"🔄 Setting things up...",
+		"📦 Fetching packages...",
+		"⚙️ Configuring project...",
+		"🔧 Building modules...",
+		"🛠️ Preparing environment...",
+		"⏳ This might take a minute, hang tight...",
+		"🙌 Almost there, just a moment...",
+		"🔍 Checking everything...",
+		"🚀 Finalizing setup...",
+	}
+	go func() {
+		for {
+			for _, suffix := range Suffixes {
+				s.Suffix = " " + suffix
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+	s.Start()
+
+	err = installCmd.Run()
+
+	// Stop the spinner
+	s.Stop()
+
 	if err != nil {
-		fmt.Println("⚠️ Oops! Something went wrong while cloning the repository. Check your connection and try again.")
+		fmt.Println("⚠️ Oops! Something went wrong while installing dependencies. Please try again.")
 		os.Exit(1)
 	}
 
